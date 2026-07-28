ba09569ecfc1a8997c8c7537b35cfa453e35fddc
letov7482-ui <letov7482@gmail.com>
2026-07-28 21:40:35 +0300

    Create HitEffectMode.java

---
+++ src/main/java/com/aethor/visuals/cosmetics/hiteffect/HitEffectMode.java
@@ -0,0 +1,10 @@
+package com.aethor.visuals.cosmetics.hiteffect;
+
+public enum HitEffectMode {
+    STARS,       // жёлтые звёзды разлетаются от точки удара
+    SPARKS,      // электро-искры
+    HEARTS,      // розовые сердечки (мемный режим)
+    SNOWFLAKES,  // снежинки, кружатся и медленно падают
+    EXPLOSION,   // мини-взрыв частиц во все стороны
+    RUNES        // фиолетовые магические руны, вращаются и растворяются
+}
